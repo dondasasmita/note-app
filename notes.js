c0f85ed7b85d09802f32eeb06bc3aef7291767dc
dondasasmita@gmail.com <dondasasmita@gmail.com>
2018-05-20 13:28:21 +0800

    added functionality to prevent duplicate title

--- notes.js
+++ notes.js
@@ -1,8 +1,7 @@
 console.log('Starting note.js')
 const fs = require('fs')
 
-const addNote =  (title, body) => {
-    console.log('adding your note')      
+const addNote =  (title, body) => {     
     //variable to store notes in an array
     let notes = []
     
@@ -12,24 +11,33 @@ const addNote =  (title, body) => {
         body
     }
 
-    
-
     //try to write if file exists and catches the error if it does not
     try {
         //fetching and store the notes array from the file to the variable 
         let notesString = fs.readFileSync('note-data.json')
         //parse to JSON
-        notes = JSON.parse(notesString)
-        //add note object to the notes array
+        notes = JSON.parse(notesString)    
+    }
+    catch (e){
+        console.log('writing file and adding your note')  
+            //add note object to the notes array
+            notes.push(note)
+            //write the notes in String type to the specified file 
+            fs.writeFileSync('note-data.json', JSON.stringify(notes))
+    }
+    //filter note title, store in the object duplicate notes if title exists
+    let duplicateNotes = notes.filter((note) => note.title === title)
+
+    //if title does not exist, duplicateNotes array is 0
+    if (duplicateNotes.length === 0) {
+        // add note object to the notes array
         notes.push(note)
         //write the notes in String type to the specified file 
         fs.writeFileSync('note-data.json', JSON.stringify(notes))
-        
+        console.log('Note succesfully added')
+    } else {
+        console.log('Note cannot be added')
     }
-    catch (e){
-        console.log("File does not exists.")
-    }
-    console.log('note added')
 }
 
 const removeNote = (title) => {
