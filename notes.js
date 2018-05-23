aedf3e70391a03dba3fb663dd90e0fd0caad214b
dondasasmita@gmail.com <dondasasmita@gmail.com>
2018-05-23 20:14:53 +0800

    refactoring codes - fetch and save

--- notes.js
+++ notes.js
@@ -1,30 +1,40 @@
 console.log('Starting note.js')
 const fs = require('fs')
 
+const fetchNotes = () => {
+    //try to write if file exists and catches the error if it does not
+    try {
+        //fetching and store the notes array from the file to the variable 
+        let notesString = fs.readFileSync('note-data.json')
+        //parse to JSON
+        return notes = JSON.parse(notesString)    
+    }
+    catch (e){
+        console.log('writing file and adding your note')  
+        //add note object to the notes array
+        notes.push(note)
+        //write the notes in String type to the specified file 
+        fs.writeFileSync('note-data.json', JSON.stringify(notes))
+        return notes = JSON.parse(notesString) 
+    }
+
+}
+
+const saveNotes = (notes) => {
+     //write the notes in String type to the specified file 
+     fs.writeFileSync('note-data.json', JSON.stringify(notes))
+}
+
 const addNote =  (title, body) => {     
     //variable to store notes in an array
-    let notes = []
+    let notes = fetchNotes()
     
     //note object
     let note = {
         title,
         body
     }
-
-    //try to write if file exists and catches the error if it does not
-    try {
-        //fetching and store the notes array from the file to the variable 
-        let notesString = fs.readFileSync('note-data.json')
-        //parse to JSON
-        notes = JSON.parse(notesString)    
-    }
-    catch (e){
-        console.log('writing file and adding your note')  
-            //add note object to the notes array
-            notes.push(note)
-            //write the notes in String type to the specified file 
-            fs.writeFileSync('note-data.json', JSON.stringify(notes))
-    }
+    
     //filter note title, store in the object duplicate notes if title exists
     let duplicateNotes = notes.filter((note) => note.title === title)
 
@@ -32,8 +42,7 @@ const addNote =  (title, body) => {
     if (duplicateNotes.length === 0) {
         // add note object to the notes array
         notes.push(note)
-        //write the notes in String type to the specified file 
-        fs.writeFileSync('note-data.json', JSON.stringify(notes))
+        saveNotes(notes)
         console.log('Note succesfully added')
     } else {
         console.log('Note cannot be added')
